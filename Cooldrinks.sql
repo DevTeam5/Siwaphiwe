446c07b104938065ec65c9f99a1f6f946b0394cd
phylius <61042832+phylius@users.noreply.github.com>
2020-02-19 11:24:54 +0200

    Add files via upload

---
+++ Cooldrinks.sql
@@ -0,0 +1,6 @@
+items,Types,"Selling Price","Stock Price",Quantity
+Cooldrinks,Coke,19,17,250
+Cooldrinks,Sprite,17,14,120
+Cooldrinks,Fanta,18,14,250
+Cooldrinks,Jive,15,11.5,140
+Cooldrinks,Schweppes,20,15,90
