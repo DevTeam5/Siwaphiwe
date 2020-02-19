6550c488b8280437e3d473f8047363c2d7874d63
phylius <61042832+phylius@users.noreply.github.com>
2020-02-19 11:20:45 +0200

    Add files via upload

---
+++ Cereal.sql
@@ -0,0 +1,6 @@
+items,Types,"Selling Price","Stock Price",Quantity,Barcode
+Cereals,Weetbix,29,19,25,15478956243
+Cereals,Cornflakes,35,20.9,20,147258369
+Cereals,Futurelife,35,22.5,25,1011172835
+Cereals,JungleOats,30,25.33,30,85214569873
+Cereals,"Rice krispies",30,23,35,555999888444111333
