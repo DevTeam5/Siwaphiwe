11b149bf42a8f17062bec68a90fea47fedc3be7d
phylius <61042832+phylius@users.noreply.github.com>
2020-02-19 11:27:25 +0200

    Add files via upload

---
+++ veggies.sql
@@ -0,0 +1,6 @@
+items,Types,"Selling Price","Stock Price",Quantity,Barcode
+Veggies,Carrots,22,11.5,25,1475825
+Veggies,Pumpkin,27,19,20,123456789
+Veggies,Broccoli,28,18.5,15,2578965545
+Veggies,Cabbage,25,19,17,23453745346
+Veggies,Potatoes,30,25,45,2454547897
