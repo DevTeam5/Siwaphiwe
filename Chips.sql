bfaeb28c2674d49ce5770e1069393d5a9c81c900
phylius <61042832+phylius@users.noreply.github.com>
2020-02-19 11:22:23 +0200

    Add files via upload

---
+++ Chips.sql
@@ -0,0 +1,11 @@
+items,Types,"Selling Price","Stock Price",Quantity
+chips,lays,18,10,200
+chips,simba,20,14,180
+chips,doritos,19,11,45
+chips,nikanaks,15,10.5,90
+chips,cornitos,11,9.5,65
+Cereals,Weetbix,29,19,25
+Cereals,Cornflakes,35,20.9,20
+Cereals,Futurelife,35,22.5,25
+Cereals,JungleOats,30,25.33,30
+Cereals,"Rice krispies",30,23,35
