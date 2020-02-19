3ae1c8d0ffc307b1134dab9fbb4b264e659f1b90
phylius <61042832+phylius@users.noreply.github.com>
2020-02-19 11:26:07 +0200

    Add files via upload

---
+++ cupcakes.sql
@@ -0,0 +1,11 @@
+items,Types,"Selling Price","Stock Price",Quantity
+Cupcakes,Velvet,29,17,25
+Cupcakes,Vanilla,23,30,26
+Cupcakes,Chocolate,20,27,15
+Cupcakes,Blueberry,20,35,10
+Cupcakes,Chocmint,27,35,10
+Cupcakes,Velvet,29,17,25
+Cupcakes,Vanilla,23,30,26
+Cupcakes,Chocolate,20,27,15
+Cupcakes,Blueberry,20,35,10
+Cupcakes,Chocmint,27,35,10
